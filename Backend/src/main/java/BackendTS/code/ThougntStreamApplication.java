130f7b31af23338318bfd5a6d755cce37421cbf2
atharva <atharvamk6@gmail.com>
2024-07-19 17:56:16 +0530

    resources and dependencies added

---
+++ Backend/src/main/java/BackendTS/code/ThougntStreamApplication.java
@@ -0,0 +1,13 @@
+package BackendTS.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThougntStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThougntStreamApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/BackendTS/code/ThougntStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package BackendTS.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThougntStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
